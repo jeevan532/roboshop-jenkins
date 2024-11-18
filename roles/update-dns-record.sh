d9e8db96157058d9331b1626163d38a749b285f2
roboshop <j.devops05324u@gmail.com>
2024-11-18 17:24:17 +0530

    creating jobs and folder through ansible

---
+++ roles/update-dns-record.sh
@@ -0,0 +1,13 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+echo  '
+      {
+         "Comment": "CREATE/DELETE/UPSERT a record ",
+         "Changes": [{
+           "Action": "UPSERT",
+           "ResourceRecordSet": {
+             "Name": "jenkins",
+             "Type": "A",
+             "TTL": 30,
+             "ResourceRecords": [{ "Value": "IPADDRESS"}]
+           }}]
+      }' | sed -e "s/IPADDRESS/$(IP)/"
